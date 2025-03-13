902ce44c18678b2970a94a5c3752e675791452bf
Siddhivinayakps <pssiddhivinayak@gmail.com>
2025-03-13 22:12:39 +0530

    Added scriptable objects for managing sprites used in game

---
+++ Assets/CyberCardCaliberly/Scripts/SpriteList.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpriteData", menuName = "Level Design/Sprite List")]
+public class SpriteList : ScriptableObject
+{
+    public Sprite[] sprites;
+}
